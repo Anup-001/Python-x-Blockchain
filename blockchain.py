f15a5490828eaae974e7c416dd3a03e8a97236da
Anup-c4 <anupy696@gmail.com>
2025-08-20 10:18:00 +0530

    reduced code

--- blockchain.py
+++ blockchain.py
@@ -28,20 +28,19 @@ def print_blockchain_elements():
 def verify_chain():
     block_index=0
     is_valid=True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue 
-        elif block[0] == blockchain[block_index -1]:
+        elif blockchain[block_index][0] == blockchain[block_index -1]:
             is_valid=True
         else:
             is_valid=False
-            break
-        block_index += 1
     return is_valid
+
+waiting_for_input =True
         
 
-while True:
+while waiting_for_input:
     print('Choose an option')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -57,7 +56,7 @@ while True:
         if len(blockchain)>=1:
             blockchain[0]=[2]
     elif user_choice=='q':
-        break
+        waiting_for_input=False
     else:
         print('Invalid input! Please pick a listed option.')
     if not verify_chain():
